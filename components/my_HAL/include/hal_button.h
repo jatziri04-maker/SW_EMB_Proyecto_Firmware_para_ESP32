f10491ee8e8b83419cafeeae9597d6f9a020073d
alex <abdiel.alejandro4124@gmail.com>
2026-06-07 23:08:55 -0600

    Added HAL for GPIO button and fixed a few things in GPIO driver.

---
+++ components/my_HAL/include/hal_button.h
@@ -0,0 +1,51 @@
+#ifndef HAL_BUTTON_H
+#define HAL_BUTTON_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "gpio_2026.h"
+
+// **** Secciones del archivo:
+// - Tipos de datos.
+// - Variables globales.
+// - Prototipos de funciones para configurar GPIOs como entradas o salidas.
+
+
+
+
+/**************************************************************************/
+// **** Tipos de datos:
+
+typedef struct{
+	uint8_t 	_pin;
+	uint8_t 	_state;
+	uint8_t 	_past_state;
+	uint32_t	_last_press_time_ms;
+} button_t;
+
+
+
+
+/**************************************************************************/
+// **** Variables globales:
+
+extern uint16_t DEBOUNCE_TIME_MS;
+
+
+/**************************************************************************/
+// **** Prototipos de funciones:
+
+// Función para configurar GPIO como botón:
+void button_init(button_t* btn, uint8_t pin, gpio_mode_e pull_mode, uint8_t invert_logic);
+
+// Funciones para manipular y leer estado del botón:
+bool button_get_state(button_t* btn);
+bool button_get_past_state(button_t* btn);
+void button_set_past_state(button_t* btn);
+
+// Función para identificar si el botón ha sido presionado (transición):
+bool button_was_pressed(button_t* btn);
+
+
+#endif /* HAL_BUTTON_H */
---
+++ components/my_HAL/include/hal_gpio.h
@@ -0,0 +1,6 @@
+#ifndef HAL_GPIO_H
+#define HAL_GPIO_H
+
+#include "hal_button.h"
+
+#endif /* HAL_GPIO_H */
